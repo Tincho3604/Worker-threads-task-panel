2c37b56f6c98936f751f1fff39be5dc2d488bb44
tincho3604 <tincho3604@gmail.com>
2025-11-12 22:04:12 -0300

    [Feat]: Establish socket io conection

--- backend/src/heavyTasks/task.js
+++ backend/src/heavyTasks/task.js
@@ -2,7 +2,6 @@ const { parentPort } = require('worker_threads');
 parentPort.on('message', (msg) => {
    console.log(`Mensaje recibido del hilo principal: ${msg}`);
   })
-
 let result = 0;
 const startTime = performance.now();
 for (let i = 0; i < 1e10; i++) {
