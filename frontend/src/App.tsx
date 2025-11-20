35b49d4096381b6967f8ef39bbc22743577f6a37
tincho3604 <tincho3604@gmail.com>
2025-11-20 02:40:58 -0300

    [Feat]: Delete response constant

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css'
 import { GridComponent } from './components/grid/gridComponent';
-import { response } from './utils/constants';
+import { COLUMNS_DEFAULT } from './utils/constants';
 import type { rowsProps } from './utils/interfaces';
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 function App() {
-  const [columns] = useState<string[]>(response.mainThreat.columns);
   const [rows, setRows] = useState<rowsProps[]>([]);
 
   useEffect(():any => {
@@ -17,17 +16,16 @@ function App() {
       setRows(prev => {
         const merge = [...prev, ...workers];
         const map = new Map(merge.map(item => [item.threadId, item]));
-        const result = Array.from(map.values());
-        return result;
+        const workerThreads = Array.from(map.values());
+        return workerThreads;
       });
     });
-
     return () => socket.disconnect();
   }, []);
 
   return (
     <div className='main-container-grid'>
-      <GridComponent columns={columns} rows={rows} />
+      <GridComponent columns={COLUMNS_DEFAULT} rows={rows} />
     </div>
   );
 }
